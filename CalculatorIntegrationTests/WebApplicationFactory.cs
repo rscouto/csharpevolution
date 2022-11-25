c3394649d025c6c879dc6d236420ee09b7996936
Diego Gutierrez <diegogutierrezfigueiredo@gmail.com>
2022-11-25 16:47:32 -0300

    WIP

--- CalculatorIntegrationTests/WebApplicationFactory.cs
+++ CalculatorIntegrationTests/WebApplicationFactory.cs
@@ -1,20 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using CsharpEvolution.Tests01.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 
 namespace CalculatorIntegrationTests;
 public class WebApplicationFactory 
 {
+    private readonly PerformedOperationContext _context;
     private readonly HttpClient _client;
 
-	public WebApplicationFactory()
-	{
-		var appFactory = new WebApplicationFactory<Program>();
-		_client = appFactory.CreateClient(); 
-	}
+    public WebApplicationFactory()
+    {
+        var builder = new WebHostBuilder()
+            //.UseEnvironment("Testing")
+            .UseStartup<Program>();
 
-	[Fact]	
+        var server = new TestServer(builder);
+        _context = server.Host.Services.GetService(typeof(PerformedOperationContext)) as PerformedOperationContext;
+        _client = server.CreateClient();
+    }
+
+    [Fact]	
 	public void Test()
 	{
-		var response = _client.GetAsync(_client.BaseAddress).GetAwaiter().GetResult();
+		var httpGetResult = _client.GetAsync($"\"/operations\"");
+
 	} 
 }
 
